af8d7b9a014fe4414320be72215c8fea0f519787
Wayne <wabbruscatoorr@gmail.com>
2020-03-12 15:53:52 -0400

    updating index.js file adding for each loop to axios get request

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -25,6 +25,17 @@
 */
 
 const followersArray = [];
+followersArray.forEach(element => {
+  axios
+    .get(`https://api.github.com/users`)
+    .then(response => {
+      console.log(response.data);
+      mainCard.append(cardData(response.data));
+    })
+    .catch(error => {
+      console.log("error", error);
+    });
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
